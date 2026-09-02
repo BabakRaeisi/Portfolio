e58400615dfa7ed1aee9c48b26cffae1103ce5c9
Raeisi <36035391+BabakRaeisi@users.noreply.github.com>
2026-09-02 02:23:27 -0400

    Configure router for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
 
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
 
